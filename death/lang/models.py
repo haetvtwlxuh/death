78d6b8fe125c134c7d2fcac6c2190e48a239cdc1
death <www.aiguagua@qq.com>
2018-04-23 00:20:00 +0800

    deathlang2.0

--- death/lang/models.py
+++ death/lang/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
-def DeathLang(request):
-	title=models.CharField(max_leng=100,primary_key=True)
+class DeathLang(models.Model):
+	title=models.CharField(max_length=100,primary_key=True,verbose_name="说的话")
+	date=models.DateTimeField()
+	def __str__(self):
+		return self.title
+class Langlist(models.Model):
+	name=models.CharField(max_length=10,primary_key=True,verbose_name="语言")
+	
+	def __str__(self):
+		return self.name
+		
+class LangText(models.Model):
+	title=models.CharField(max_length=100,primary_key=True)
+	data=models.DateTimeField()
+	text=models.TextField()
+	fors=models.ForeignKey(Langlist,on_delete=models.CASCADE)
 
+	def __str__(self):
+		return self.title
